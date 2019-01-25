734fb97f4e53f34bfde3c95325703c799fa00d9e
Joe <joe45moses@gmail.com>
2019-01-25 18:05:59 +0000

    initial push

---
+++ app.js
@@ -0,0 +1,35 @@
+// search hack
+$('.search-bar input').on('focus', () => ($('.search-bar').css('background', 'linear-gradient(to right, orange , yellow, green, cyan, blue, violet)')));
+$('.search-bar input').on('focusout', () => ($('.search-bar').css('background', 'none')));
+$('.ctc').click(function(e){
+	var snip = $(this).siblings('pre').text();
+	$('#clip-copy-off-screen').val(snip).select();
+	document.execCommand("copy");
+	copied_pop();
+})
+var sound = new Howl({
+  src: ['ding.mp3'],
+  volume: .2
+});
+
+
+
+
+function copied_pop(){
+	$('.pop-wrap').show();
+	$('.pop-wrap').fadeOut(1500);
+	sound.play();
+}
+
+$('input').keyup(function(e){
+	var searchTerm = $(this).val().toUpperCase();
+	var cards = $('.card-title').val();
+	// $('.card').hide();
+	if (searchTerm.length > 0) {
+		$('.card').show();
+		$('.card-title:not(:contains('+searchTerm+'))').parent().parent().hide(); 
+	} else {
+		$('.card').show();
+	}
+
+});
---
+++ index.php
@@ -0,0 +1,213 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta content="width=device-width, initial-scale=1, shrink-to-fit=no" name="viewport"><!-- Bootstrap CSS -->
+    <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" rel="stylesheet">
+    <link href="style/style.css" rel="stylesheet">
+    <title>Hello, world!</title>
+</head>
+<body>
+    <div class="pop-wrap">
+        <div class="pop-content">
+            <h1 class="page-title">Snipped!</h1>
+        </div>
+    </div>
+    <div class="heading">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12">
+                    <h1 class="page-title">Snip-it</h1>
+                    <p>Welcome to Snip-it (get it?). HTML is tiresome and repetitive. One of the most boring yet important parts of a Web Developers job... why do we do this to ourselves? Because... well... I don't know, dude. I really don't. Anyway, I made some snippits (GET IT?), take them and do as you please. Probably some cool features coming soon, who knows.</p>
+                    <div class="search-bar">
+                        <input placeholder="Search for a snippit..." type="text">
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="snips">
+        <div class="container">
+            <div class="row">
+                <div class="col-sm-12">
+                    <div class="card-columns">
+                        <div class="card">  
+                            <div class="card-body">
+                                <h5 class="card-title">DAY SELECT</h5>
+                                <button class="btn btn-sm btn-primary ctc">
+                                    Copy
+                                </button>
+<pre>
+    &lt;select name="day"&gt; 
+        &lt;option value="1"&gt;1&lt;/option&gt;
+        &lt;option value="2"&gt;2&lt;/option&gt;
+        &lt;option value="3"&gt;3&lt;/option&gt;
+        &lt;option value="4"&gt;4&lt;/option&gt;
+        &lt;option value="5"&gt;5&lt;/option&gt;
+        &lt;option value="6"&gt;6&lt;/option&gt;
+        &lt;option value="7"&gt;7&lt;/option&gt;
+        &lt;option value="8"&gt;8&lt;/option&gt;
+        &lt;option value="9"&gt;9&lt;/option&gt;
+        &lt;option value="10"&gt;10&lt;/option&gt;
+        &lt;option value="11"&gt;11&lt;/option&gt;
+        &lt;option value="12"&gt;12&lt;/option&gt;
+        &lt;option value="13"&gt;13&lt;/option&gt;
+        &lt;option value="14"&gt;14&lt;/option&gt;
+        &lt;option value="15"&gt;15&lt;/option&gt;
+        &lt;option value="16"&gt;16&lt;/option&gt;
+        &lt;option value="17"&gt;17&lt;/option&gt;
+        &lt;option value="18"&gt;18&lt;/option&gt;
+        &lt;option value="19"&gt;19&lt;/option&gt;
+        &lt;option value="20"&gt;20&lt;/option&gt;
+        &lt;option value="21"&gt;21&lt;/option&gt;
+        &lt;option value="22"&gt;22&lt;/option&gt;
+        &lt;option value="23"&gt;23&lt;/option&gt;
+        &lt;option value="24"&gt;24&lt;/option&gt;
+        &lt;option value="25"&gt;25&lt;/option&gt;
+        &lt;option value="26"&gt;26&lt;/option&gt;
+        &lt;option value="27"&gt;27&lt;/option&gt;
+        &lt;option value="28"&gt;28&lt;/option&gt;
+        &lt;option value="29"&gt;29&lt;/option&gt;
+        &lt;option value="30"&gt;30&lt;/option&gt;
+        &lt;option value="31"&gt;31&lt;/option&gt;
+    &lt;/select&gt;
+</pre> 
+                            </div>
+                        </div>
+                        <div class="card">  
+                            <div class="card-body">
+                                <h5 class="card-title">MONTH SELECT</h5>
+
+                                <button class="btn btn-sm btn-primary ctc">
+                                    Copy
+                                </button>
+<pre>
+    &lt;select name="month"&gt; 
+        &lt;option value="1"&gt;1&lt;/option&gt; 
+        &lt;option value="2"&gt;2&lt;/option&gt; 
+        &lt;option value="3"&gt;3&lt;/option&gt; 
+        &lt;option value="4"&gt;4&lt;/option&gt; 
+        &lt;option value="5"&gt;5&lt;/option&gt; 
+        &lt;option value="6"&gt;6&lt;/option&gt; 
+        &lt;option value="7"&gt;7&lt;/option&gt; 
+        &lt;option value="8"&gt;8&lt;/option&gt; 
+        &lt;option value="9"&gt;9&lt;/option&gt; 
+        &lt;option value="10"&gt;10&lt;/option&gt; 
+        &lt;option value="11"&gt;11&lt;/option&gt; 
+        &lt;option value="12"&gt;12&lt;/option&gt; 
+    &lt;/select&gt; 
+</pre>
+                            </div>
+                        </div>
+                        <div class="card">  
+                            <div class="card-body">
+                                <h5 class="card-title">D.O.B Select</h5>
+                                <button class="btn btn-sm btn-primary ctc">
+                                    Copy
+                                </button>
+<pre>
+    &lt;select name="year"&gt; 
+        &lt;option value="1940"&gt;1940&lt;/option&gt;
+        &lt;option value="1941"&gt;1941&lt;/option&gt;
+        &lt;option value="1942"&gt;1942&lt;/option&gt;
+        &lt;option value="1943"&gt;1943&lt;/option&gt;
+        &lt;option value="1944"&gt;1944&lt;/option&gt;
+        &lt;option value="1945"&gt;1945&lt;/option&gt;
+        &lt;option value="1946"&gt;1946&lt;/option&gt;
+        &lt;option value="1947"&gt;1947&lt;/option&gt;
+        &lt;option value="1948"&gt;1948&lt;/option&gt;
+        &lt;option value="1949"&gt;1949&lt;/option&gt;
+        &lt;option value="1950"&gt;1950&lt;/option&gt;
+        &lt;option value="1951"&gt;1951&lt;/option&gt;
+        &lt;option value="1952"&gt;1952&lt;/option&gt;
+        &lt;option value="1953"&gt;1953&lt;/option&gt;
+        &lt;option value="1954"&gt;1954&lt;/option&gt;
+        &lt;option value="1955"&gt;1955&lt;/option&gt;
+        &lt;option value="1956"&gt;1956&lt;/option&gt;
+        &lt;option value="1957"&gt;1957&lt;/option&gt;
+        &lt;option value="1958"&gt;1958&lt;/option&gt;
+        &lt;option value="1959"&gt;1959&lt;/option&gt;
+        &lt;option value="1960"&gt;1960&lt;/option&gt;
+        &lt;option value="1961"&gt;1961&lt;/option&gt;
+        &lt;option value="1962"&gt;1962&lt;/option&gt;
+        &lt;option value="1963"&gt;1963&lt;/option&gt;
+        &lt;option value="1964"&gt;1964&lt;/option&gt;
+        &lt;option value="1965"&gt;1965&lt;/option&gt;
+        &lt;option value="1966"&gt;1966&lt;/option&gt;
+        &lt;option value="1967"&gt;1967&lt;/option&gt;
+        &lt;option value="1968"&gt;1968&lt;/option&gt;
+        &lt;option value="1969"&gt;1969&lt;/option&gt;
+        &lt;option value="1970"&gt;1970&lt;/option&gt;
+        &lt;option value="1971"&gt;1971&lt;/option&gt;
+        &lt;option value="1972"&gt;1972&lt;/option&gt;
+        &lt;option value="1973"&gt;1973&lt;/option&gt;
+        &lt;option value="1974"&gt;1974&lt;/option&gt;
+        &lt;option value="1975"&gt;1975&lt;/option&gt;
+        &lt;option value="1976"&gt;1976&lt;/option&gt;
+        &lt;option value="1977"&gt;1977&lt;/option&gt;
+        &lt;option value="1978"&gt;1978&lt;/option&gt;
+        &lt;option value="1979"&gt;1979&lt;/option&gt;
+        &lt;option value="1980"&gt;1980&lt;/option&gt;
+        &lt;option value="1981"&gt;1981&lt;/option&gt;
+        &lt;option value="1982"&gt;1982&lt;/option&gt;
+        &lt;option value="1983"&gt;1983&lt;/option&gt;
+        &lt;option value="1984"&gt;1984&lt;/option&gt;
+        &lt;option value="1985"&gt;1985&lt;/option&gt;
+        &lt;option value="1986"&gt;1986&lt;/option&gt;
+        &lt;option value="1987"&gt;1987&lt;/option&gt;
+        &lt;option value="1988"&gt;1988&lt;/option&gt;
+        &lt;option value="1989"&gt;1989&lt;/option&gt;
+        &lt;option value="1990"&gt;1990&lt;/option&gt;
+        &lt;option value="1991"&gt;1991&lt;/option&gt;
+        &lt;option value="1992"&gt;1992&lt;/option&gt;
+        &lt;option value="1993"&gt;1993&lt;/option&gt;
+        &lt;option value="1994"&gt;1994&lt;/option&gt;
+        &lt;option value="1995"&gt;1995&lt;/option&gt;
+        &lt;option value="1996"&gt;1996&lt;/option&gt;
+        &lt;option value="1997"&gt;1997&lt;/option&gt;
+        &lt;option value="1998"&gt;1998&lt;/option&gt;
+        &lt;option value="1999"&gt;1999&lt;/option&gt;
+        &lt;option value="2000"&gt;2000&lt;/option&gt;
+        &lt;option value="2001"&gt;2001&lt;/option&gt;
+        &lt;option value="2002"&gt;2002&lt;/option&gt;
+        &lt;option value="2003"&gt;2003&lt;/option&gt;
+        &lt;option value="2004"&gt;2004&lt;/option&gt;
+        &lt;option value="2005"&gt;2005&lt;/option&gt;
+        &lt;option value="2006"&gt;2006&lt;/option&gt;
+        &lt;option value="2007"&gt;2007&lt;/option&gt;
+        &lt;option value="2008"&gt;2008&lt;/option&gt;
+        &lt;option value="2009"&gt;2009&lt;/option&gt;
+        &lt;option value="2010"&gt;2010&lt;/option&gt;
+        &lt;option value="2011"&gt;2011&lt;/option&gt;
+        &lt;option value="2012"&gt;2012&lt;/option&gt;
+        &lt;option value="2013"&gt;2013&lt;/option&gt;
+        &lt;option value="2014"&gt;2014&lt;/option&gt;
+        &lt;option value="2015"&gt;2015&lt;/option&gt;
+        &lt;option value="2016"&gt;2016&lt;/option&gt;
+        &lt;option value="2017"&gt;2017&lt;/option&gt;
+        &lt;option value="2018"&gt;2018&lt;/option&gt;
+    &lt;/select&gt; 
+</pre>  
+                            </div>
+                        </div>
+                        
+                        <div class="card">  
+                            <div class="card-body">
+                                <h5 class="card-title">D.O.B Select</h5>
+                                <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <textarea id="clip-copy-off-screen"></textarea>
+ <script src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.11.0/umd/popper.min.js" integtity="sha384-b/U6ypiBEHpOf/4+1nzFpr53nxSS+GLCkfwBdFNTxtclqqenISfwAzpKaMNFNmj4" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/js/bootstrap.min.js" integtity="sha384-h0AbiXch4ZDo7tp9hKZ4TsHbi047NrKGLO3SEJAg45jXxnGIfYzk4Si90RDIqNm1" crossorigin="anonymous"></script>
+    <script src="node_modules/howler/dist/howler.js"></script>
+    <script src="app.js">
+    </script>
+</body>
+</html>
